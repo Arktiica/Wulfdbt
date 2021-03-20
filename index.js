606b72c96dd26a44d70e31aa704e76046bf30c1a
Noah Thomas <n.coburn.thomas@gmail.com>
2021-03-20 15:47:34 -0500

    Stuff to add for index to work.

--- index.js
+++ index.js
@@ -49,9 +49,22 @@ client.login(token);
 
 
 
-/* Don't forget that you need a config.json with the Prefix & Token. You'll may also need
-to get the node_modules. Additional information for what you may need, how to get these,
-or documentation for discord.js & JavaScript can be found in the following: 
+/* Don't forget you'll need the token if you copy the entire code include
+the index. To get the token you'll need to go to https://discord.com/developers
+and get the token for your bot. With this create a new file named: config.json and
+input the following code:
+
+{
+    "token": "TOKEN_ID_HERE",
+    "prefix": "DESIRED_PREFIX"
+}
+
+Note that the prefix will be "hard coded" and cannot be changed via discord with
+what is all here.
+
+Additional information for what you may need, how to get these,
+or documentation for discord.js & JavaScript can be found in the following:
+    https://discord.com/developers
     https://discordjs.guide/#before-you-begin
     https://discord.js.org/#/
     https://developer.mozilla.org/en-US/docs/Web/JavaScript
